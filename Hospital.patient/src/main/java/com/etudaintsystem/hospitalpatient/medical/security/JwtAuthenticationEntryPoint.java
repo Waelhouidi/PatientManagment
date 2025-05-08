e362583c7ed1258f6dccd8c0e075b9f25fc59a24
Waelhouidi <waelhwidi0@gmail.com>
2025-05-08 01:23:20 +0100

    Add services , controller and dto

---
+++ Hospital.patient/src/main/java/com/etudaintsystem/hospitalpatient/medical/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.etudaintsystem.hospitalpatient.medical.security;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Non autorisé: " + authException.getMessage());
+    }
+}
---
+++ Hospital.patient/src/main/java/com/etudaintsystem/hospitalpatient/medical/security/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.etudaintsystem.hospitalpatient.medical.security;
+
+public class JwtAuthenticationFilter {
+}
