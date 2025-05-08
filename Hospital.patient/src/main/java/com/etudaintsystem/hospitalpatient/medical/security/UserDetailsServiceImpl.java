57ba24b26f49a0fb1dc09ebc6be3cc54467eadc6
Waelhouidi <waelhwidi0@gmail.com>
2025-05-08 01:31:26 +0100

    Add services , controller and dto

--- Hospital.patient/src/main/java/com/etudaintsystem/hospitalpatient/medical/security/UserDetailsServiceImpl.java
+++ Hospital.patient/src/main/java/com/etudaintsystem/hospitalpatient/medical/security/UserDetailsServiceImpl.java
@@ -1,4 +1,27 @@
 package com.etudaintsystem.hospitalpatient.medical.security;
 
-public class UserDetailsServiceImpl {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.etudaintsystem.hospitalpatient.medical.model.User;
+import com.etudaintsystem.hospitalpatient.medical.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé avec le nom d'utilisateur: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
 }
